2c440ba91be286579b21a36486ab4ddec2c0d318
MalikaLaouiti <laouiti.malika@yahoo.com>
2026-04-22 13:44:25 +0100

    configuration BD connection

--- src/DataBase/DataBaseConfig.java
+++ src/DataBase/DataBaseConfig.java
@@ -1,4 +1,11 @@
 package DataBase;
 
 public class DataBaseConfig {
+    public static final String NOM_DRIVER = "org.postgresql.Driver";
+    public static final String IPServer="localhost";
+    public static final String PORT= "5020";
+    public static final String DataBaseName="Files";
+    public static final String URL_DB ="jdbc:postgresql://"+IPServer+":"+PORT+"/"+DataBaseName;
+    public static final String USERNAME="postgres";
+    public static final String PASSWORD="malikaadmin";
 }
--- src/DataBase/DataBaseConnection.java
+++ src/DataBase/DataBaseConnection.java
@@ -1,4 +1,25 @@
 package DataBase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DataBaseConnection {
+    private static Connection instance = null;
+
+    private void DatabaseConnection() {}
+
+    public static Connection getInstance() throws SQLException {
+        if (instance == null || instance.isClosed()) {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("Driver PostgreSQL introuvable. Ajoute postgresql-xx.jar au classpath.", e);
+            }
+            instance = DriverManager.getConnection(DataBaseConfig.URL_DB, DataBaseConfig.USERNAME, DataBaseConfig.PASSWORD);
+            System.out.println("[DB] Connexion PostgreSQL établie.");
+        }
+        return instance;
+    }
+
 }
+//j'ai utilisé design pattern :singleton
